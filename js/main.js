37a5c7d0700a0518ae704adcf2cf1815628ba6f2
Amjad Ali <mjhomefolder@Amjads-MacBook-Pro.local>
2018-03-08 10:26:44 -0600

    created gasme logic

--- js/main.js
+++ js/main.js
@@ -22,6 +22,7 @@ var cards = [
 ];
 var cardsInPlay = [];
 function checkForMatch(){
+	
 	if(cardsInPlay.length === 2){
 		if(cardsInPlay[0] === cardsInPlay[1]){
 			alert("You found a match!");
@@ -29,16 +30,25 @@ function checkForMatch(){
 		else{alert("Sorry, try again.");}
 	}
 }
-function flipCard (cardId){
 
+function flipCard (){
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src',cards[cardId].cardImage);
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].suit);
-	console.log(cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
-
-	
-	
 }
-flipCard(0);
-flipCard(1);
+
+function createBoard(){
+	for(var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src','images/back.png');
+		cardElement.setAttribute('data-id',[i]);
+		cardElement.addEventListener("click",flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+
+	}
+}
+createBoard();
+
